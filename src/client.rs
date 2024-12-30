f5e26bd7de83f19f6f6f28cbe5768c0b1386a069
hedon954 <171725713@qq.com>
2024-12-30 18:32:52 +0800

    feat(tls): support accpet and connect with tls for server and client

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,18 @@
-use kvdb::{CommandRequest, ProstClientStream};
+use kvdb::{CommandRequest, ProstClientStream, TlsClientConnector};
 use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
+    let ca_cert = include_str!("../fixtures/ca.cert");
+
     // connect to server
     let addr = "127.0.0.1:9527";
+
+    let connector = TlsClientConnector::new("kvserver.acme.inc", None, Some(ca_cert))?;
     let stream = TcpStream::connect(addr).await?;
+    let stream = connector.connect(stream).await?;
 
     // create client stream
     let mut client = ProstClientStream::new(stream);
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use kvdb::{MemTable, ProstServerStream, Service, ServiceInner};
+use kvdb::{MemTable, ProstServerStream, Service, ServiceInner, TlsServerAcceptor};
 use tokio::net::TcpListener;
 use tracing::{error, info};
 
@@ -7,14 +7,27 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let addr = "127.0.0.1:9527";
+
+    let server_cert = include_str!("../fixtures/server.cert");
+    let server_key = include_str!("../fixtures/server.key");
+
+    let acceptor = TlsServerAcceptor::new(server_cert, server_key, None)?;
     let service: Service = ServiceInner::new(MemTable::new()).into();
     let listener = TcpListener::bind(addr).await?;
     info!("start server at {}", addr);
 
     loop {
+        let tls = acceptor.clone();
         let (socket, addr) = listener.accept().await?;
         info!("accept connection from {}", addr);
-        let stream = ProstServerStream::new(socket, service.clone());
+        let stream = match tls.accept(socket).await {
+            Ok(stream) => stream,
+            Err(e) => {
+                error!("failed to accept connection: {}", e);
+                continue;
+            }
+        };
+        let stream = ProstServerStream::new(stream, service.clone());
         tokio::spawn(async move {
             if let Err(e) = stream.process().await {
                 error!("failed to process connection: {}", e);
